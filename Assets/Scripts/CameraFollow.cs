86481ef2d581ccf38c088a13cde0cc5269024b5f
Evan Konig <Evan Konig>
2016-05-11 15:01:49 -0400

    This is the intial commit of code to the git repository

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float m_speed = 0.1f;
+	Camera mycam;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		mycam = GetComponent<Camera> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		mycam.orthographicSize = (Screen.height / 100f) / 4f;
+
+		if (target) {
+			/* m_speed determines how quickly the camera starts up and stops when the target stops moving
+			 * the -10 for Z moves the camera out so it is not literally on the player
+			 */
+			
+			transform.position = Vector3.Lerp (transform.position, target.position, m_speed) + new Vector3(0,0,-10);
+
+	}
+}
+}
---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+
+	public Transform warpTarget;
+
+	IEnumerator OnTriggerEnter2D(Collider2D other){
+
+		Screenfader sf = GameObject.FindGameObjectWithTag ("Fader").GetComponent<Screenfader> ();
+
+		yield return StartCoroutine (sf.FadeToBlack ());
+
+		other.gameObject.transform.position = warpTarget.position;
+		Camera.main.transform.position = warpTarget.position;
+
+
+		yield return StartCoroutine (sf.FadeToClear ());
+	}
+}
